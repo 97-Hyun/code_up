94b034756a0ff3de4b598af9beb0225cdc189c11
hyun <kimhyun15372@gmail.com>
2025-02-04 19:11:25 +0900

    [feat] 6010 문제 해결

---
+++ 6010-take_integer_and_print_it_out/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * [기초 입출력] 정수 1개 입력받아 그대로 출력하기
+ */
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(Integer.parseInt(st.nextToken()));
+    }
+}
